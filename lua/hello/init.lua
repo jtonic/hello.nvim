5029622e9d7938775aad9da8dcbfde831c70009e
tony <antonel-ernest.pazargic@ing.com>
2024-08-19 21:43:51 +0300

    Minor change

--- lua/hello/init.lua
+++ lua/hello/init.lua
@@ -1,9 +1,11 @@
 local M = {}
 
 function M.hello()
-	vim.notify("Hello, World!!!", vim.log.levels.OFF, { title = "Message" })
+	vim.notify("Hello, Tony!!!", vim.log.levels.OFF, { title = "Message" })
 end
 
-vim.api.nvim_create_user_command("Hello", M.hello, {})
+function M.setup()
+	vim.api.nvim_create_user_command("Hello", M.hello, {})
+end
 
 return M
